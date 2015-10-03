6b18a48637a18965b868239385ae8a08c7437e30
rizkyoktaviani96 <orizky64@gmail.com>
2015-10-03 11:16:50 +0700

    Tugas merubah warna
    rizky oktaviani_1431140053
    management informatika

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.DarkBlue Then
+            Me.BackColor = Color.Brown
+        Else
+            Me.BackColor = Color.DarkBlue
+        End If
+    End Sub
+End Class
